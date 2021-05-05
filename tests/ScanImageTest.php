1d14747d3d7cf4a5120a93a9ff5693f5aee101c9
Brian Rogers <captbrogers@gmail.com>
2021-05-05 09:49:52 -0600

    Adding basic failing test

---
+++ tests/ScanImageTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Captbrogers\TesseractOcrTests;
+
+use PHPUnit\Framework\TestCase;
+
+final class ScanImageTest extends TestCase
+{
+    public function testCanScanImage(): void
+    {
+        $this->assertTrue(false);
+    }
+}
